a6e0c90c27e051d7acd87b7afc2b8efb56ec7e2a
oreninjas <bjbjbjbj2600@gmail.com>
2024-12-26 19:06:43 +0000

    Added dynamic data | Dashboard

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,25 @@
 import { useNavigate } from "react-router-dom";
 
-const Card = ({ title, description }) => {
+const Card = ({ title, description, isPublished }) => {
   //   const navigate = useNavigate();
 
-  //   const handleCardClick = () => {
-  //     if (linkifyId) {
-  //       navigate(`/linkify/${linkifyId}`);
-  //     }
-  //   };
+  const handleCardClick = () => {
+    if (linkifyId) {
+      navigate(`/linkify/${linkifyId}`);
+    }
+  };
 
   return (
     <div
-      className="flex-shrink-0 w-60 h-72 rounded-[20px] bg-zinc-900/50 text-white p-4 cursor-pointer hover:bg-zinc-800 transition"
-      //   onClick={handleCardClick}
+      className="w-full md:w-1/6 h-1/4 md:h-50 bg-zinc-900/50 text-white p-4 cursor-pointer rounded"
+      onClick={handleCardClick}
     >
       <div className="mt-4">
         <h3 className="text-lg font-semibold">{title}</h3>
         <p className="text-sm mt-2 text-gray-400">{description}</p>
       </div>
       <div className="mt-4 flex justify-between text-xs text-gray-500">
-        {/* <span>{isPublished ? "Published" : "Private"}</span> */}
-        idk published or no
+        <span>{isPublished ? "Published" : "Private"}</span>
       </div>
     </div>
   );
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,41 +2,21 @@ import { useEffect, useState } from "react";
 import axios from "../config/axios.js";
 import { useNavigate } from "react-router-dom";
 import Background from "../components/Background.jsx";
-import Card from "../components/Card.jsx";
+import Baseground from "../components/Baseground.jsx";
 
 const Dashboard = () => {
-  const [linkifies, setLinkifies] = useState([]);
-
   const navigate = useNavigate();
 
   useEffect(() => {
     axios.get("/api/auth/protected").catch(() => {
       navigate("/");
     });
-
-    const linkifies = axios
-      .get("/api/linkify/userlinkifies")
-      .then((res) => {
-        console.log(res);
-        setLinkifies(res.data);
-      })
-      .catch(() => {
-        navigate("/");
-      });
   }, []);
 
   return (
-    <div className="relative w-full h-screen bg-zinc-800">
+    <div className="relative w-full h-screen bg-zinc-800 z-[-50]">
       <Background />
-      <div>
-        {linkifies.length > 0 ? (
-          linkifies.map((items, key) => {
-            return <Card title={items.title} description={items.description} />;
-          })
-        ) : (
-          <p>There's no data</p>
-        )}
-      </div>
+      <Baseground />
     </div>
   );
 };
