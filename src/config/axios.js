822b4d2c1a2ae10b3ccfb1084309a4ff06fbeb57
oreninjas <bjbjbjbj2600@gmail.com>
2024-12-28 13:13:00 +0000

    Actualy backend url | rendered

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstanse = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://be-wld2.onrender.com/",
   withCredentials: true,
 });
 
